5025d57d8590b732d919e823aac9d0d56d80c8a1
Gregory Haynes <greg@greghaynes.net>
2014-11-14 12:27:29 -0800

    Support mail summaries

--- mailase/controllers/root.py
+++ mailase/controllers/root.py
@@ -1,7 +1,8 @@
+from elasticsearch.client import Elasticsearch
+from pecan import abort
 from pecan.rest import RestController
 from wsmeext.pecan import wsexpose
 
-from elasticsearch.client import Elasticsearch
 from email.parser import Parser as EmailParser
 import os
 
@@ -14,40 +15,42 @@ class Mailbox(object):
         self.id = id
 
 
-class Thread(object):
-
-    mail_ids = [str]
-
-
-class Mail(object):
+class MailSummary(object):
 
     id = str
     mailbox_id = str
     subject = str
-    thread_id = str
+    href = str
 
     def __init__(self, id, mailbox_id, subject):
         self.id = id
         self.mailbox_id = mailbox_id
         self.subject = subject
+        self.href = "http://localhost:8080/mail/%s/%s" % (self.mailbox_id, self.id)
+
+
+class Mail(MailSummary):
+
+    def __init__(self, id, mailbox_id, subject):
+        super(Mail, self).__init__(id, mailbox_id, subject)
 
 
 class MailSearchResult(object):
 
     query = str
-    mail_ids = [str]
+    mails = [MailSummary]
 
-    def __init__(self, query, mail_ids):
+    def __init__(self, query, mails):
         self.query = query
-        self.mail_ids = mail_ids
+        self.mails = mails
 
 
 def get_mail_by_id(mailbox_id, mail_id):
     valid = False
     for subdir in ('cur', 'tmp', 'new'):
         mail_path = "/home/greghaynes/Mail_testing/%s/%s/%s" % (mailbox_id,
-                                                                subdir,
-                                                                mail_id)
+                                                                   subdir,
+                                                                   mail_id)
         try:
             os.stat(mail_path)
             valid = True
@@ -56,7 +59,7 @@ def get_mail_by_id(mailbox_id, mail_id):
             pass
     
     if not valid:
-        return 'No valid path found'
+        return None
 
     with open(mail_path) as email_fp:
         parser = EmailParser()
@@ -81,9 +84,10 @@ class MailSearchController(RestController):
     @wsexpose(MailSearchResult, str)
     def get(self, query):
         es = Elasticsearch([{'host': 'localhost'}])
-        search_res = es.search("emails", q=query, fields="_id", size=100)
-        mail_ids = [x['_id'] for x in search_res['hits']['hits']]
-        return MailSearchResult(query, mail_ids)
+        search_res = es.search("emails", q=query, fields="subject", size=100)
+        hits = search_res['hits']['hits']
+        mails = [MailSummary(x['_id'], '.INBOX', x['fields']['subject'][0]) for x in hits]
+        return MailSearchResult(query, mails)
 
 
 class MailController(RestController):
@@ -92,7 +96,7 @@ class MailController(RestController):
 
     @wsexpose(Mail, str, str)
     def get(self, mailbox_id, mail_id):
-        return get_mail_by_id(mailbox_id, mail_id)
+        return get_mail_by_id(mailbox_id, mail_id) or abort(404)
 
 
 class RootController(object):
