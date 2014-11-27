e27b528b44cba76781241437635807279bdd758c
Gregory Haynes <greg@greghaynes.net>
2014-11-27 10:51:27 -0800

    Added bootstrap and root api

--- mailase/controllers/root.py
+++ mailase/controllers/root.py
@@ -15,56 +15,76 @@ class Mailbox(object):
         self.id = id
 
 
-class MailSummary(object):
+class Mail(object):
 
     id = str
     mailbox_id = str
+    from_ = str
     subject = str
-    href = str
+    text_payloads = [str]
 
-    def __init__(self, id, mailbox_id, subject):
+    def __init__(self, id, mailbox_id, from_ = None, subject = None):
         self.id = id
         self.mailbox_id = mailbox_id
+        msg = self.msg
+        self.from_ = from_
         self.subject = subject
-        self.href = "http://localhost:8080/mail/%s/%s" % (self.mailbox_id, self.id)
+        if msg:
+            self.from_ = self.from_ or msg['from']
+            self.subject = self.subject or msg['subject']
 
+    @property
+    def path(self):
+        valid = False
+        mail_path_temp = "/home/greghaynes/Mail_testing/%s/%s/%s"
+        for subdir in ('cur', 'tmp', 'new'):
+            mail_path = mail_path_temp % (self.mailbox_id, subdir, self.id)
+            try:
+                os.stat(mail_path)
+                valid = True
+                break
+            except OSError:
+                pass
+    
+        if not valid:
+            return None
 
-class Mail(MailSummary):
+        return mail_path
 
-    def __init__(self, id, mailbox_id, subject):
-        super(Mail, self).__init__(id, mailbox_id, subject)
+    @property
+    def msg(self):
+        mail_path = self.path
+        if mail_path is None:
+            return None
+
+        with open(mail_path) as email_fp:
+            parser = EmailParser()
+            msg = parser.parse(email_fp)
+        return msg
+
+    def exists(self):
+        return self.msg is not None
+
+    def load_text_payloads(self):
+        msg = self.msg
+        if msg is None:
+            return
+
+        text_payloads = []
+        for part in msg.walk():
+            if part.get_content_type() in ['text/plain']:
+                text_payloads.append(part.as_string())
+        self.text_payloads = text_payloads
 
 
 class MailSearchResult(object):
 
     query = str
-    mails = [MailSummary]
+    mails = [Mail]
 
     def __init__(self, query, mails):
         self.query = query
         self.mails = mails
-
-
-def get_mail_by_id(mailbox_id, mail_id):
-    valid = False
-    for subdir in ('cur', 'tmp', 'new'):
-        mail_path = "/home/greghaynes/Mail_testing/%s/%s/%s" % (mailbox_id,
-                                                                   subdir,
-                                                                   mail_id)
-        try:
-            os.stat(mail_path)
-            valid = True
-            break
-        except OSError:
-            pass
-    
-    if not valid:
-        return None
-
-    with open(mail_path) as email_fp:
-        parser = EmailParser()
-        msg = parser.parse(email_fp)
-        return Mail(mail_id, mailbox_id, msg['subject'])
 
 
 class MailboxController(RestController):
@@ -84,9 +104,13 @@ class MailSearchController(RestController):
     @wsexpose(MailSearchResult, str)
     def get(self, query):
         es = Elasticsearch([{'host': 'localhost'}])
-        search_res = es.search("emails", q=query, fields="subject", size=100)
+        search_res = es.search("emails", q=query, fields="from,info_flags,subject", size=100)
         hits = search_res['hits']['hits']
-        mails = [MailSummary(x['_id'], '.INBOX', x['fields']['subject'][0]) for x in hits]
+
+        from_ = lambda x: x['fields']['from'][0]
+        subject = lambda x: x['fields']['subject'][0]
+        info_flags = lambda x: x['fields']['info_flags'][0]
+        mails = [Mail("%s:%s" % (x['_id'], info_flags(x)), '.INBOX', from_(x), subject(x)) for x in hits]
         return MailSearchResult(query, mails)
 
 
@@ -96,7 +120,12 @@ class MailController(RestController):
 
     @wsexpose(Mail, str, str)
     def get(self, mailbox_id, mail_id):
-        return get_mail_by_id(mailbox_id, mail_id) or abort(404)
+        mail = Mail(mail_id, mailbox_id)
+        if not mail.exists():
+            abort(404)
+        else:
+            mail.load_text_payloads()
+            return mail
 
 
 class RootController(object):
