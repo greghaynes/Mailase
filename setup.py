70589daa1e4984e0e7cb1b599df5ff81b15888ae
Gregory Haynes <greg@greghaynes.net>
2014-11-27 11:50:06 -0800

    Make API app and pbr

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-# -*- coding: utf-8 -*-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='mailase',
-    version='0.1',
-    description='',
-    author='',
-    author_email='',
-    install_requires=[
-        "pecan",
-    ],
-    test_suite='mailase',
-    zip_safe=False,
-    include_package_data=True,
-    packages=find_packages(exclude=['ez_setup'])
-)
+setup(setup_requires=['pbr'],
+      pbr=True)
